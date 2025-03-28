9c7326ee82756b94df14ec82452b249c284a1a52
mariapelizzari <mpelizzari@mail.austral.edu.ar>
2025-03-28 12:48:07 -0300

    Update change.py

--- change.py
+++ change.py
@@ -2,10 +2,8 @@ def change():
     expense = 23.75
     money = 100
     gasto = float(input("Ingrese gasto: \n"))
-    print(gasto)
 
     recibo = float(input("Dinero recibido \n"))
-    print(recibo)
 
     pesos = recibo-gasto 
     centavos = (float(pesos)-int(pesos))*100
