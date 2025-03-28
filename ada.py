2e4c7376699ba05d2dadab59890f5ed77ada731d
mariapelizzari <mpelizzari@mail.austral.edu.ar>
2025-03-28 12:08:08 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,9 +1,8 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
-    full_name = f"{first_name} {last_name}"
-
-    print(full_name.lower())
-    print(full_name.title())
-    print(full_name.upper())
-    print("\t\t",full_name.lower())
+    
+    print(f"{first_name.lower()} {last_name.lower()}")
+    print(f"{first_name.title()} {last_name.title()}")
+    print(f"{first_name.upper()} {last_name.upper()}")
+    print(f"\t\t {first_name.lower()} {last_name.lower()}")
